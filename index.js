b2ff68bf73baf7124069ab1f0fa68b5accb84eb4
Brandon <oallelsefailo@gmail.com>
2023-05-01 07:26:57 -0700

    Added new tictactoe html Included JS and updated CSS

--- index.js
+++ index.js
@@ -1,19 +1,87 @@
-window.addEventListener('load', function() {
-    document.getElementById('scrollToTopBtn').style.display = 'none';
-  });
-  
-  window.addEventListener('scroll', function() {
-    if (window.pageYOffset > (document.documentElement.clientHeight * 0.30)) {
-      document.getElementById('scrollToTopBtn').style.display = 'block';
-    } else {
-      document.getElementById('scrollToTopBtn').style.display = 'none';
-    }
-  });
-  
-  document.getElementById('scrollToTopBtn').addEventListener('click', function() {
+/* Scrol to the top */
+window.addEventListener("load", function () {
+  document.getElementById("scrollToTopBtn").style.display = "none";
+});
+
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > document.documentElement.clientHeight * 0.3) {
+    document.getElementById("scrollToTopBtn").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopBtn").style.display = "none";
+  }
+});
+
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   });
-  
+
+/*Tic Tac Toe Game */
+const gameBoard = document.querySelector(".game-board");
+const cells = document.querySelectorAll(".cell");
+let currentPlayer = "X";
+
+cells.forEach((cell) => {
+  cell.addEventListener("click", handleCellClick);
+});
+
+function handleCellClick(event) {
+  const cell = event.target;
+  cell.textContent = currentPlayer;
+  cell.style.display = "flex";
+  cell.style.justifyContent = "center";
+  cell.style.alignItems = "center";
+
+  if (checkWin()) {
+    alert(currentPlayer + " wins!");
+    if (confirm("Do you want to play again?")) {
+      resetBoard();
+    }
+  } else if (checkTie()) {
+    alert("Tie game!");
+    if (confirm("Do you want to play again?")) {
+      resetBoard();
+    }
+  } else {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+  }
+}
+
+function checkWin() {
+  const winningCombinations = [
+    [0, 1, 2], // horizontal top row
+    [3, 4, 5], // horizontal middle row
+    [6, 7, 8], // horizontal bottom row
+    [0, 3, 6], // vertical left column
+    [1, 4, 7], // vertical middle column
+    [2, 5, 8], // vertical right column
+    [0, 4, 8], // diagonal top left to bottom right
+    [2, 4, 6], // diagonal top right to bottom left
+  ];
+
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cells[index].textContent === currentPlayer;
+    });
+  });
+}
+
+function checkTie() {
+  return Array.from(cells).every((cell) => {
+    return cell.textContent !== "";
+  });
+}
+
+function resetBoard() {
+  cells.forEach((cell) => {
+    cell.textContent = "";
+    cell.style.display = "";
+    cell.style.justifyContent = "";
+    cell.style.alignItems = "";
+  });
+  currentPlayer = "X";
+}
